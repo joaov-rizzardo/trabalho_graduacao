55ec28948b4601883531c4cf5a3d8766f3eadbc9
João Victor Domingues Rizzardo <jvitor2000@hotmail.com>
2023-04-29 10:31:51 -0300

    Classe de contas

--- backend/src/Models/BillInstallment.ts
+++ backend/src/Models/BillInstallment.ts
@@ -1,3 +1,68 @@
+import BillDAO from "../DAO/BillDAO"
+import getCurrentStringDatetime from "../Utils/DateUtils"
+import { ErrorLogger } from "../Utils/Logger"
+
+type BillInstallmentType = {
+    installmentId?: number
+    billId: number
+    installmentNumber: number
+    value: number
+    dueDate: string
+    isPayed: boolean
+    payedAt?: string
+    createdAt: string   
+}
 export default class BillInstallment {
-    
+    private installmentId?: number
+    private billId: number
+    private installmentNumber: number
+    private value: number
+    private dueDate: string
+    private isPayed: boolean
+    private payedAt?: string
+    private createdAt: string
+    private isExpired: boolean
+    private billDAO: BillDAO
+
+    constructor(params: BillInstallmentType){
+        this.installmentId = params.installmentId
+        this.billId = params.billId
+        this.installmentNumber = params.installmentNumber
+        this.value = params.value
+        this.dueDate = params.dueDate
+        this.isPayed = params.isPayed
+        this.payedAt = params.payedAt
+        this.createdAt = params.createdAt
+        this.isExpired = false
+        this.billDAO = new BillDAO()
+
+        this.checkIfIsExpired()
+    }
+
+    public async save(){
+
+    }
+
+    public pay(){
+        this.isPayed = true
+        this.payedAt = getCurrentStringDatetime()
+    }
+
+    private checkIfIsExpired(){
+        try {
+            const expireDate = new Date(this.dueDate)
+            const currentDate = new Date()
+            if(currentDate > expireDate){
+                this.isExpired = true
+            }else {
+                this.isExpired = false
+            }
+        }catch(error: any){
+            ErrorLogger.error(error.stack)
+        } 
+    }
+
+    private isCreated(){
+        return this.installmentId !== undefined
+    }
 }
